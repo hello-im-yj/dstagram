379e359ca4847839e5ac279180a65d860e60b1de
YujinKim-Sciedu <sandwich17yj@likelion.org>
2020-11-14 23:58:24 +0900

    20/11/09 : 클론코딩#2 completed

---
+++ photo/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from .views import *
+
+app_name = 'photo'
+urlpatterns = [
+    path("create/",PhotoCreate.as_view(),name='create'),
+    path("like/<int:photo_id>/", PhotoLike.as_view(), name = 'like'),
+    path("favorite/<int:photo_id>/", PhotoFavorite.as_view(), name = 'favorite'),
+    
+    path("update/<int:pk>",PhotoUpdate.as_view(), name='update'),
+    path("delete/<int:pk>",PhotoDelete.as_view(), name='delete'),
+    path("detail/<int:pk>",PhotoDetail.as_view(), name='detail'),
+    path("", PhotoList.as_view(), name='index'),
+]
+
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
